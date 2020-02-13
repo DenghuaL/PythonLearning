adc6e1278e3f56195911dd6720b77dea0de9f644
Denghua Lei <leidenghua@buaa.edu.cn>
2020-02-13 21:20:46 +0100

    finish 02

---
+++ 02/rec_function.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+#--*-- coding: utf-8 --*--
+
+import math
+
+def fact(n):
+    return fact_iter(n,1)
+
+def fact_iter(num,product):
+    if num == 1:
+        return product
+    else:
+        return fact_iter(num-1, num*product)
+
+test = 5
+
+print('fact(%d) = ' %test, fact(test))
+
+
+def move(n, a, b, c):
+    if  n == 1:
+        print(a, '-->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+    return 0
+
+move(3, 'A', 'B', 'C')
